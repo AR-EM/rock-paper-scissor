764d6c6116053fead6994f678ccc0538f0ce9e07
AR-EM <178697567+AR-EM@users.noreply.github.com>
2025-07-24 19:43:27 +0530

    Removed unnecessary if statements in event listener.

--- script.js
+++ script.js
@@ -74,6 +74,14 @@ const result = document.querySelector("div");
 let humanScore;
 let computerScore;
 humanScore = computerScore = 0;
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+    playRound(button.textContent, getComputerChoice());
+    });
+})
+
+
 function scoreCounter(score){
     if (score === 1){
         humanScore++;
@@ -87,50 +95,6 @@ function scoreCounter(score){
         result.textContent += `... You: ${humanScore} Computer: ${computerScore}`;
     }
 }
-
-
-buttons.forEach((button) => {
-    if (humanScore < 5 && computerScore < 5){
-        button.addEventListener("click", () => {
-        playRound(button.textContent, getComputerChoice());
-    })
-    };
-})
-
-
-
-
-
-// function playGame(){
-//     let humanScore;
-//     let computerScore;
-
-//     humanScore = computerScore = 0;
-
-    
-
-//     for (let i=0;i<5;i++){
-//         const humanChoice = getHumanChoice();
-//         console.log("Your choice: ", humanChoice);
-//         const computerChoice = getComputerChoice();
-//         console.log("Computer's choice: ",computerChoice);
-
-//         let whoWon = playRound(humanChoice, computerChoice);
-
-//         if (whoWon == 1) humanScore++;
-//         else if (whoWon==0) computerScore++;
-
-//        if (humanScore==3 || computerScore==3) break; 
- 
-//     if (humanScore>computerScore){
-//         console.log("You won!");
-//     }
-//     else console.log("Computer won!");
-// }}
-
-
-
-
 
 
 
